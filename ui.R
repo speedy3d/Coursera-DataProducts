ed2b6fdaffa39ea82e9e8db1fdb59e9a58cd03f6
speedy3d <ryan@speedy3d.com>
2015-06-20 16:31:48 -0400

    Final update - added instructions

--- ui.R
+++ ui.R
@@ -41,6 +41,11 @@ shinyUI(pageWithSidebar(
                           ),
                   
                 tabPanel("Documentation", value="Table", 
+                          h3('Instructions'),
+                          p('1) Choose whether you would like to use imperial or metric units using the dropdown box in the upper left corner'),
+                          p('2) Use the sliders to enter your weight (in lg or kg), and height (in inches or centimeters)'),
+                          p('3) Hit the submit button to show your current BMI and weight status'),
+                          p('4) You can view a chart with your weight plotted against acceptable weight ranges using the comparison chart tab'),
                           h3('Formula used in imperial calculations'),
                           p('BMI = Weight (in lb)/Height^2 (in inches) * 703'),
                           code('w/h^2 * 703'),
@@ -53,7 +58,7 @@ shinyUI(pageWithSidebar(
                           p('Overweight = 25-29.9'),
                           p('Obesity = BMI of 30 or greater'),
                           p(''),
-                          h5("Reference:", a("CDC", href="http://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html?s_cid=tw_ob064"))
+                          h5("Reference:", a("The CDC", href="http://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html?s_cid=tw_ob064"))
                           )
                  
     )
